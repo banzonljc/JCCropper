bb812cf14dcfebce18f145a949761a8e684a0f4f
banzonljc <banzonljc@itri.org.tw>
2016-09-15 06:54:39 +0800

    Initial commit

---
+++ JCCropper/JCCropper/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  JCCropper
+//
+//  Created by banzon on 9/15/16.
+//  Copyright © 2016 banzon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
